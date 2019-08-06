9025cefcd27b5e0735f0e900b5cff7b1ddde3d4f
Bathiya Seneviratne <bathiyaseneviratne@bathiyas-MacBook-Pro.local>
2019-08-06 10:47:40 +0530

    Create face detection method and Main.py for activate camera for capture the video

---
+++ Main.py
@@ -0,0 +1,20 @@
+import numpy as np
+import cv2
+import faceDetector as fd
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    ret, frame = cap.read()
+    facecount, frame = fd.faceDetection(frame)
+    print("Faces in scene ",facecount)
+
+    for(x,y,w,h) in facecount:
+        cv2.rectangle(frame,(x,y),(x+w, y+h),(0,0,255),thickness=2)
+
+    cv2.imshow("Facetrack",frame)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+    
+cap.release()
+cv2.destroyAllWindows()
