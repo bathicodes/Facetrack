1eb79e6cd30d069d3b0cac550d9b80678bfc93e8
Bathiya Seneviratne <bathiyaseneviratne@bathiyas-MacBook-Pro.local>
2019-08-06 19:23:44 +0530

    Delete unused packages

--- Main.py
+++ Main.py
@@ -1,11 +1,9 @@
 import numpy as np
 import cv2
 import faceDetector as fd
-# import serial
 
 cap = cv2.VideoCapture(0)
 
-# ser = serial.Serial('/dev/cu.usbmodem141201',9600)
 
 
 while(True):
@@ -16,8 +14,6 @@ while(True):
 
     for(x,y,w,h) in facecount:
         cv2.rectangle(frame,(x,y),(x+w, y+h),(0,0,255),thickness=2)
-        # ser.write(1)
-
 
     cv2.imshow("Facetrack",frame)
     if cv2.waitKey(1) & 0xFF == ord("q"):
