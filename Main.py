fd3d8f06f587cf62c5f04bbcaa196e6265e2e71b
Bathiya Seneviratne <seneviratne.bathiya@gmail.com>
2019-08-23 20:29:14 +0530

    remove unwanted trash and make the original repos back

--- Main.py
+++ Main.py
@@ -5,18 +5,12 @@ from playsound import playsound
 
 cap = cv2.VideoCapture(0)
 
-faceinside = False
 
 while(True):
     ret, frame = cap.read()
     facecount, frame = fd.faceDetection(frame)
     print(facecount)
     print("Faces in scene ",len(facecount))
-
-    if len(facecount) > 3:
-        faceinside = True
-        if faceinside == True:
-            playsound('w.mp3')
 
     for(x,y,w,h) in facecount:
         cv2.rectangle(frame,(x,y),(x+w, y+h),(0,0,255),thickness=2)
@@ -25,9 +19,6 @@ while(True):
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
     
-
-# if faceinside == True:
-#     playsound('test.mp3')
 
 
 cap.release()
--- test.py
+++
@@ -1,3 +0,0 @@
-from playsound import playsound
-
-playsound('test.mp3')
